c5c3892b3cdeecf159271a42b04f85197af4c11a
boshinomikoto <104365113+boshinomikoto@users.noreply.github.com>
2025-03-10 20:46:24 +0200

    changes

---
+++ hello.asm
@@ -0,0 +1,25 @@
+global _start
+
+section .data
+message: db "less than zero", 10
+len equ $ -message
+section .text
+
+_start:
+    mov rbx, 10;
+    mov rsi, -10
+    add rsi, rbx
+    js escape
+    mov rdi, rsi
+    jmp exit
+
+escape:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, len
+    syscall
+
+exit:
+    mov rax, 60
+    syscall
